a3b9536c36ee6c3b55d572e17afffba76be920cd
pablogrsc <37184817+pablogrsc@users.noreply.github.com>
2021-04-04 19:20:06 +0200

    Update config.py

--- SNMP/config.py
+++ SNMP/config.py
@@ -2,6 +2,6 @@ import os
 
 base_path = os.path.join(os.path.dirname(__file__))
 
-community = 'castellar'
+community = 'xx'
 linux_target_addresses = ['<<ip>>']
 windows_target_addresses = ['<<ip>>,<<ip2>>']
